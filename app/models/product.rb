77a46894c82c09648b52cdf536a3c92cb501ca73
aluno <aluno@aluno.com>
2017-03-30 09:17:00 -0300

    Regra e teste para impedir deletar produto referenciado por carrinho

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,7 +11,20 @@
 #
 
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Nao pode excluir. LineItem presente.')
+      throw :abort
+    end
+  end
+
 end
